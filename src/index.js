6e328ac643689335665071d652d2e7b277b43270
doubleface <christophe@cozycloud.cc>
2018-04-12 22:01:26 +0200

    Add sentry dsn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// Force sentry DSN into environment variables
+// In the future, will be set by the stack
+process.env.SENTRY_DSN =
+  process.env.SENTRY_DSN ||
+  'https://89d41b28d3844f1bb53fa010ea78503c:75dc52afdcca41aca4e1351c80affc6c@sentry.cozycloud.cc/33'
+
 const { BaseKonnector } = require('cozy-konnector-libs')
 const { authenticate } = require('./auth')
 const helpers = require('./helpers')
