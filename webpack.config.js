8013179749d33be56a0f61b82067e99421296e8f
doubleface <christophe@cozycloud.cc>
2018-04-17 18:31:26 +0200

    feat: add registry publishing in travis + konitor

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const entry = require('./package.json').main
 module.exports = {
   entry,
   target: 'node',
+  mode: 'none',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'index.js'
@@ -14,6 +15,7 @@ module.exports = {
       { from: 'manifest.konnector' },
       { from: 'package.json' },
       { from: 'README.md' },
+      { from: 'assets' },
       { from: 'LICENSE' }
     ])
   ]
