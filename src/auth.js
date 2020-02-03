dba2237df6566f99872a8bc64b908532797392f8
doubleface <christophe@cozycloud.cc>
2020-02-03 18:09:58 +0100

    fix: avoid scraping detection

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,6 @@
 const { log, errors } = require('cozy-konnector-libs')
 const { rootUrl, request } = require('./request')
+const sleep = require('util').promisify(global.setTimeout)
 
 const loginUrl = rootUrl + '/espace_client/connexion'
 
@@ -8,22 +9,30 @@ module.exports = {
 }
 
 // Darty temporarily disables an account after 5 login failures.
-function authenticate(login, password) {
+async function authenticate(login, password) {
   log('info', 'Authenticating...')
+  try {
+    await request.get('https://www.darty.com/espace_client/connexion')
+    await sleep(10000)
+    await request.post({
+      uri: loginUrl,
+      form: {
+        email: login,
+        password: password
+      },
+      headers: {
+        referer: 'https://www.darty.com/espace_client/connexion',
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+        Pragma: 'no-cache',
+        'Cache-Control': 'no-cache'
+      }
+    })
 
-  return request({
-    method: 'POST',
-    uri: loginUrl,
-    form: {
-      email: login,
-      password: password
-    }
-  })
-    .then(() => {
-      log('info', 'Successfully logged in')
-    })
-    .catch(err => {
-      if (err.statusCode === 401) throw new Error(errors.LOGIN_FAILED)
-      else throw err
-    })
+    log('info', 'Successfully logged in')
+  } catch (err) {
+    if (err.statusCode === 401) throw new Error(errors.LOGIN_FAILED)
+    else throw err
+  }
 }
--- src/request.js
+++ src/request.js
@@ -6,7 +6,10 @@ const request = requestFactory({
   // debug: true,
   cheerio: true,
   jar: true,
-  json: false
+  json: false,
+  headers: {
+    'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3'
+  }
 })
 
 module.exports = {
