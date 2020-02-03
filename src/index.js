2b1ee0df4ff5f249629b05f9b5acd40779f1df94
doubleface <christophe@cozycloud.cc>
2020-02-03 17:14:52 +0100

    chore: packaging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// Force sentry DSN into environment variables
-// In the future, will be set by the stack
 process.env.SENTRY_DSN =
   process.env.SENTRY_DSN ||
   'https://89d41b28d3844f1bb53fa010ea78503c:75dc52afdcca41aca4e1351c80affc6c@sentry.cozycloud.cc/33'
